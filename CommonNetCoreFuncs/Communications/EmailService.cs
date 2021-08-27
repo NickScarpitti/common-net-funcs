22de91c0f14f23df92c33fe2ad8cd57d84169933
Nick Scarpitti <nick_scarpitti@na.honda.com>
2021-08-27 11:30:26 -0400

    Updated email functions to use filestream

--- CommonNetCoreFuncs/Communications/EmailService.cs
+++ CommonNetCoreFuncs/Communications/EmailService.cs
@@ -4,14 +4,14 @@ namespace CommonNetCoreFuncs.Communications
 {
     public interface IEmailService
     {
-        bool SendEmail(MailAddress from, MailAddress to, string toName, string toEmail, string subject, string body, MailAddress cc = null, string attachmentName = null, MemoryStream fileData = null);
+        bool SendEmail(MailAddress from, MailAddress to, string toName, string toEmail, string subject, string body, bool bodyIsHtml, MailAddress cc = null, string attachmentName = null, FileStream fileData = null);
     }
 
     public class EmailService : IEmailService
     {
-        public bool SendEmail(MailAddress from, MailAddress to, string toName, string toEmail, string subject, string body, MailAddress cc = null, string attachmentName = null, MemoryStream fileData = null)
+        public bool SendEmail(MailAddress from, MailAddress to, string toName, string toEmail, string subject, string body, bool bodyIsHtml, MailAddress cc = null, string attachmentName = null, FileStream fileData = null)
         {
-            return Email.SendEmail(from, to, subject, body, cc, attachmentName, fileData);
+            return Email.SendEmail(from, to, subject, body, bodyIsHtml, cc, attachmentName, fileData);
         }
     }
 }

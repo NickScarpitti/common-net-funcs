1a011fe383b8fb64010bf57c2822dd4f4bf14f10
Nick <nick.scarpitti@gmail.com>
2022-06-22 16:55:40 -0400

    Enabled implicit usings

--- CommonNetCoreFuncs/Communications/Email.cs
+++ CommonNetCoreFuncs/Communications/Email.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using MailKit.Net.Smtp;
 using MimeKit;
 

b72661c83b60c28844ffea66c5ef76ff0418787a
Nick Scarpitti <nick_scarpitti@na.honda.com>
2022-08-09 15:07:47 -0400

    Added Ceiling methods similar to Excel formula in new MathHelpers class

---
+++ CommonNetCoreFuncs/Tools/MathHelpers.cs
@@ -0,0 +1,28 @@
+﻿namespace CommonNetCoreFuncs.Tools;
+
+public static class MathHelpers
+{
+    public static double Ceiling(this double? value, double significance)
+    {
+        value ??= 0;
+
+        if ((value % significance) != 0)
+        {
+            return ((int)(value / significance) * significance) + significance;
+        }
+
+        return Convert.ToDouble(value);
+    }
+
+    public static decimal Ceiling(this decimal? value, decimal significance)
+    {
+        value ??= 0;
+
+        if ((value % significance) != 0)
+        {
+            return ((int)(value / significance) * significance) + significance;
+        }
+
+        return Convert.ToDecimal(value);
+    }
+}

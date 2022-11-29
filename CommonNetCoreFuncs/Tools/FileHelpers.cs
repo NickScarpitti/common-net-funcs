977e706bcbc140bd9010750ecada3e3341223db5
Nick Scarpitti <nick_scarpitti@na.honda.com>
2022-11-29 10:22:01 -0500

    Added zip functions to FileHelpers

--- CommonNetCoreFuncs/Tools/FileHelpers.cs
+++ CommonNetCoreFuncs/Tools/FileHelpers.cs
@@ -1,10 +1,14 @@
-﻿namespace CommonNetCoreFuncs.Tools;
+﻿using System.IO.Compression;
+
+namespace CommonNetCoreFuncs.Tools;
 
 /// <summary>
 /// Helpers for dealing with files
 /// </summary>
 public static class FileHelpers
 {
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
     /// <summary>
     /// Simulates automatic Windows behavior of adding a number after the original file name when a file with the same name exists already
     /// </summary>
@@ -38,4 +42,65 @@ public static class FileHelpers
             return originalFullFileName;
         }
     }
+
+    public static async Task AddFileToZip(this ZipArchive archive, ZipFile? zipFile, int fileCount)
+    {
+        try
+        {
+            if (zipFile?.FileData != null)
+            {
+                zipFile.FileData.Position = 0; //Must have this to prevent errors writing data to the attachment
+                    var entry = archive.CreateEntry(zipFile.FileName ?? $"File {fileCount}", CompressionLevel.SmallestSize);
+                    using var entryStream = entry.Open();
+                    await zipFile.FileData.CopyToAsync(entryStream);
+                    await entryStream.FlushAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "AddFileToZip Error");
+        }
+    }
+    
+    public static async Task ZipFiles(IEnumerable<ZipFile>? zipFiles, MemoryStream? zipFileStream = null)
+    {
+        try
+        {
+            zipFileStream ??= new();
+
+            if (zipFiles != null && zipFiles.Any())
+            {
+                int i = 1;
+                using MemoryStream memoryStream = new();
+                using ZipArchive archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true);
+                foreach (ZipFile file in zipFiles)
+                {
+                    if (file.FileData != null)
+                    {
+                        file.FileData.Position = 0; //Must have this to prevent errors writing data to the attachment
+                        var entry = archive.CreateEntry(file.FileName ?? $"File {i}", CompressionLevel.SmallestSize);
+                        using var entryStream = entry.Open();
+                        await file.FileData.CopyToAsync(entryStream);
+                        await entryStream.FlushAsync();
+                        i++;
+                    }
+                }
+                archive.Dispose();
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                await memoryStream.CopyToAsync(zipFileStream); //Copy to output stream
+                zipFileStream.Seek(0, SeekOrigin.Begin);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "ZipFiles Error");
+        }
+    }
+}
+
+
+public class ZipFile
+{
+    public Stream? FileData { get; set; }
+    public string? FileName { get; set; }
 }

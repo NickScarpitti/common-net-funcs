3b0eb2eb03f4bc2c37891776a65dd808f9d1817e
Nick Scarpitti <nick_scarpitti@na.honda.com>
2023-02-15 13:01:52 -0500

    Added ParseCamelCase to StringManipulation class Added definition for RestObject in RestHelpers to make its use more apparent

--- CommonNetCoreFuncs/Web/RestHelpers.cs
+++ CommonNetCoreFuncs/Web/RestHelpers.cs
@@ -8,6 +8,10 @@ using NLog;
 
 namespace CommonNetCoreFuncs.Web;
 
+/// <summary>
+/// Helper class to get around not being able to pass primitive types directly to a generic type
+/// </summary>
+/// <typeparam name="T">Primitive type to pass to the REST request</typeparam>
 public class RestObject<T> where T : class
 {
     public T? Result { get; set; }

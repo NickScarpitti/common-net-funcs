195c5a848604ddd6ff6e4585e581c2c56da1b5fb
Nick Scarpitti <nick_scarpitti@na.honda.com>
2023-02-28 14:58:50 -0500

    Updated HtmlBuilder class

--- CommonNetCoreFuncs/Communications/HtmlBuilder.cs
+++ CommonNetCoreFuncs/Communications/HtmlBuilder.cs
@@ -1,106 +1,141 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Web;
 
 namespace CommonNetCoreFuncs.Communications;
 
 public static class HtmlBuilder
 {
-    public static string BuildHtmlString(string body, string footer, List<List<string>>? tableData = null)
+    /// <summary>
+    /// Creates an HTML body for an email using the inputs provided
+    /// </summary>
+    /// <param name="body">Main text body of the email that goes before a table if there is one</param>
+    /// <param name="footer">Any text to be displayed under the table or after the body</param>
+    /// <param name="tableData">Data to be formatted into a table. First item should be the header data</param>
+    /// <returns></returns>
+    public static string BuildHtmlEmail(string body, string? footer = null, List<List<string>>? tableData = null)
     {
         string text = "<html><body>";
 
-        text += StringtoHtml(body);
-        text += CreateHtmlTable(tableData);
-        text += StringtoHtml(footer);
-        FormatAllUrlsToHtml(ref text);
+        text += body.StringtoHtml();
+        text += "<br><br>";
+        text += tableData.CreateHtmlTable();
+        text += !string.IsNullOrWhiteSpace(footer) ? "<br><br>" : string.Empty;
+        text += footer.StringtoHtml();
+        text = text.FormatAllUrlsToHtml();
 
         text += "</body></html>";
         return text;
     }
 
-    public static string StringtoHtml(string text)
+    /// <summary>
+    /// Replace line breaks with <br> and tabs with &nbsp&nbsp&nbsp to be HTML compatible
+    /// </summary>
+    /// <param name="text">String to be formatted</param>
+    /// <returns></returns>
+    public static string StringtoHtml(this string? text)
     {
-        //replace linebreaks
-        text = HttpUtility.HtmlEncode(text);
-        text = text.Replace("\r\n", "\r");
-        text = text.Replace("\n", "\r");
-        text = text.Replace("\r", "<br>");
-
-
-        //replace tabs
-        text = text.Replace("\t", "&nbsp&nbsp&nbsp");
-
-        return text;
-
+        if (text != null) 
+        { 
+            text = HttpUtility.HtmlEncode(text);
+            text = text.Replace("\r\n", "\r").Replace("\n", "\r").Replace("\r", "<br>").Replace("\t", "&nbsp&nbsp&nbsp");
+        }
+        return text ?? string.Empty;
     }
 
-
-    private static void FormatAllUrlsToHtml(ref string text)
+    /// <summary>
+    /// Use regex to find all URLs, ASSUMES URL ENDS WITH WHITESPACE
+    /// </summary>
+    /// <param name="text">Text to search for and format urls in</param>
+    /// <param name="linkText">Text to display for link</param>
+    /// <returns></returns>
+    private static string FormatAllUrlsToHtml(this string text, string? linkText = null)
     {
-        // use regexpression to find all of URLs, ASSUMES URL ENDS WITH WHITESPACE
         Regex regx = new Regex(@"https?://[^\n\t< ]+", RegexOptions.IgnoreCase);
         MatchCollection matches = regx.Matches(text);
-        foreach (Match url in matches)
+        foreach (Match url in matches.AsEnumerable())
         {
-            text = text.Replace(url.Value, CreateHtmlLink(url.Value));
+            text = text.Replace(url.Value, url.Value.CreateHtmlLink(linkText ?? "Click Here"));
         }
-    }
-
-    private static string CreateHtmlLink(string url, string linkText = "Click Here")
-    {
-        string text = $"<a href=\"{url}\">{linkText}</a>";
         return text;
     }
 
-    private static string CreateHtmlTable(List<List<string>>? tableData, bool applyFancyStyle = true)
+    /// <summary>
+    /// Creates a link behind the given text value
+    /// </summary>
+    /// <param name="url">Url to embed behind he text</param>
+    /// <param name="linkText">Text to display for link</param>
+    /// <returns></returns>
+    private static string CreateHtmlLink(this string url, string linkText = "Click Here")
     {
-        string text = "<br><br>";
-        string tableStyle =
-            "<style>table{font-family: arial, sans-serif;border-collapse: collapse;width: 100%;}td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}tr:nth-child(even) {background - color: #dddddd;}</style>";
+        return $"<a href=\"{url}\">{linkText}</a>";
+    }
 
-        if (tableData == null || tableData.Count <= 0)
+    /// <summary>
+    /// Create an HTML table from given data
+    /// </summary>
+    /// <param name="tableData">Data to turn into an HTML table</param>
+    /// <param name="applyTableCss">Apply CSS styling to table</param>
+    /// <param name="customCss">Custom CSS to apply to the table. If not provided, default will be used.</param>
+    /// <returns></returns>
+    private static string CreateHtmlTable(this List<List<string>>? tableData, bool applyTableCss = true, string? customCss = null)
+    {
+        string tableHtml = string.Empty;
+        string tableStyle = !string.IsNullOrWhiteSpace(customCss) ? customCss : 
+            "<style>" +
+                "table{" +
+                    "font-family: arial, sans-serif;" +
+                    "border-collapse: collapse;" +
+                    "width: 100%;" +
+                "}" +
+                "td, th {" +
+                    "border: 1px solid #dddddd;" +
+                    "text-align: left;" +
+                    "padding: 8px;" +
+                "}" +
+                "tr:nth-child(even) {" +
+                    "background - color: #dddddd;" +
+                "}" +
+            "</style>";
+
+        if (tableData == null || !tableData.Any())
         {
-            return text;
+            return tableHtml;
         }
 
-        if (applyFancyStyle)
-        {
-            text += tableStyle;
-        }
+        tableHtml += applyTableCss ? tableStyle : string.Empty;
 
         List<string> tableHeaders = tableData[0];
 
-        //make headers
-        text += "<table><tr>";
-        foreach (var hdr in tableHeaders)
+        //Make headers
+        tableHtml += "<table><tr>";
+        foreach (string header in tableHeaders)
         {
-            text += "<th>" + hdr + "</th>";
+            tableHtml += $"<th>{header}</th>";
         }
-        text += "</tr>";
+        tableHtml += "</tr>";
 
-        //add data rows
-        foreach (var row in tableData)
+        //Add data rows
+        bool firstLoop = true;
+        foreach (List<string> rowData in tableData)
         {
-            if (row == tableHeaders)
+            if (!firstLoop)
             {
+                tableHtml += "<tr>";
+                foreach (string rowItem in rowData)
+                {
+                    tableHtml += $"<td>{rowItem}</td>";
+                }
+                tableHtml += "</tr>";
+            }
+            else
+            {
+                firstLoop= false;
                 continue;
             }
-            text += "<tr>";
-            foreach (var dataItem in row)
-            {
-                text += "<td>" + dataItem + "</td>";
-            }
-            text += "</tr>";
         }
 
-        text += "</table>";
-
+        tableHtml += "</table>";
         
-        return text;
+        return tableHtml;
     }
 }

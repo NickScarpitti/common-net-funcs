c519235ad802b2430a925bc2666e5b4d0780d12f
Nick Scarpitti <nick_scarpitti@na.honda.com>
2023-03-02 14:51:37 -0500

    Updated file names to match new repos

---
+++ common-net-funcs/Web/JWT/TokenObject.cs
@@ -0,0 +1,8 @@
+﻿namespace Common_Net_Funcs.Web.JWT;
+
+public class TokenObject
+{
+	public string? Token { get; set; }
+	public string? RefreshToken { get; set; }
+    public DateTime? JwtExpireTime { get; set; }
+}

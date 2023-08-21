e725c37ad8b2959d497b33acc063af255184a8d7
Nick Scarpitti <nick_scarpitti@na.honda.com>
2023-08-21 16:23:23 -0400

    Updated APIs to use generic endpoints Moved generic API helpers to CommonNetFuncs

---
+++ common-net-funcs/Web/BaseActions/GenericEndpoints.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Common_Net_Funcs.Tools;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common_Net_Funcs.Web.BaseActions;
+public class GenericEndpoints : ControllerBase
+{
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public async Task<ActionResult<List<T>>> CreateMany<T, UT>(IEnumerable<T> models, IBaseDbContextActions<T, UT> baseAppDbContextActions) where T : class where UT : DbContext
+    {
+        try
+        {
+            await baseAppDbContextActions.CreateMany(models);
+            if (await baseAppDbContextActions.SaveChanges())
+            {
+                return Ok(models);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"{MethodBase.GetCurrentMethod()?.Name} Error");
+        }
+        return NoContent();
+    }
+
+    public async Task<ActionResult<T>> Delete<T, UT>(T model, IBaseDbContextActions<T, UT> baseAppDbContextActions) where T : class where UT : DbContext
+    {
+        try
+        {
+            baseAppDbContextActions.DeleteByObject(model);
+            if (await baseAppDbContextActions.SaveChanges())
+            {
+                return Ok(model);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"{MethodBase.GetCurrentMethod()?.Name} Error");
+        }
+        return NoContent();
+    }
+
+    public async Task<ActionResult<List<T>>> DeleteMany<T, UT>(IEnumerable<T> models, IBaseDbContextActions<T, UT> baseAppDbContextActions) where T : class where UT : DbContext
+    {
+        try
+        {
+            if (models.Any())
+            {
+                baseAppDbContextActions.DeleteMany(models);
+                if (await baseAppDbContextActions.SaveChanges())
+                {
+                    return Ok(models);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"{MethodBase.GetCurrentMethod()?.Name} Error");
+        }
+        return NoContent();
+    }
+
+    public async Task<ActionResult<T>> Patch<T, UT>(object primaryKey, JsonPatchDocument<T> patch, IBaseDbContextActions<T, UT> baseAppDbContextActions) where T : class where UT : DbContext
+    {
+        try
+        {
+            T? dbModel = await baseAppDbContextActions.GetByKey(primaryKey);
+            if (dbModel == null)
+            {
+                return NoContent();
+            }
+
+            if (patch.Operations.Count == 0)
+            {
+                return Ok(dbModel);
+            }
+
+            T? updateModel = dbModel.DeepClone();
+
+            if (updateModel == null)
+            {
+                return NoContent();
+            }
+
+            patch.ApplyTo(updateModel);
+
+            List<ValidationResult> failedValidations = new();
+            Validator.TryValidateObject(updateModel, new(updateModel), failedValidations);
+            if (failedValidations.Any())
+            //if (!TryValidateModel(updateModel)) //Only works when this method is the controller endpoint being called
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            updateModel.CopyPropertiesTo(dbModel);
+            baseAppDbContextActions.Update(dbModel);
+            if (await baseAppDbContextActions.SaveChanges())
+            {
+                return Ok(dbModel);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"{MethodBase.GetCurrentMethod()?.Name} Error");
+        }
+        return NoContent();
+    }
+}
---
+++ common-net-funcs/Web/BaseActions/IBaseDbContextActions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common_Net_Funcs.Web.BaseActions;
+public interface IBaseDbContextActions<T, UT> where T : class where UT : DbContext
+{
+    #region Read
+    Task<T?> GetByKey(object primaryKey);
+    Task<List<T>?> GetAll();
+    Task<List<T>?> GetWithFilter(Expression<Func<T, bool>> expression);
+    Task<T?> GetOneWithFilter(Expression<Func<T, bool>> expression);
+    #endregion
+
+    #region Write
+    Task Create(T model);
+    Task CreateMany(IEnumerable<T> model);
+    void DeleteByObject(T model);
+    Task DeleteByKey(object id);
+    void DeleteMany(IEnumerable<T> model);
+    void Update(T model);
+    void UpdateMany(List<T> models);
+    Task<bool> SaveChanges();
+    #endregion
+}

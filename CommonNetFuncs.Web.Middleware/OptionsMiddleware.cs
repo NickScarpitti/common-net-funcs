1b88a8ea9f7d2b24b43aa0be310824c5e3516757
Nick Scarpitti <nick.scarpitti@gmail.com>
2025-02-19 19:01:36 -0500

    Some attempts to make auth work

---
+++ CommonNetFuncs.Web.Middleware/OptionsMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace CommonNetFuncs.Web.Middleware;
+
+// Custom middleware for OPTIONS requests
+public static class OptionsMiddlewareExtensions
+{
+    public static IApplicationBuilder UseOptions(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<OptionsMiddleware>();
+    }
+}
+
+public class OptionsMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public OptionsMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Method.StrComp("OPTIONS"))
+        {
+            context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers["Origin"].ToString());
+            context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+            context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            context.Response.StatusCode = 200;
+            return;
+        }
+
+        await _next(context);
+    }
+}

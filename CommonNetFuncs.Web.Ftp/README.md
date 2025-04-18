f38a187476487eb82676a740cb763a9aa7884e3b
Nick Scarpitti <nick_scarpitti@na.honda.com>
2025-04-18 11:57:56 -0400

    Refactor SFTP and CSV handling; add new project
    - Updated `MemoryCacheMiddleware` to implement proper disposal pattern.
    - Refactored `SshFtp` class with extension methods for better SFTP management.
    - Added `FileTransferConnection` class to encapsulate connection details.
    - Introduced `CsvReadHelpers` for reading CSV files using `CsvHelper`.
    - Added new `CommonNetFuncs.Web.Ftp` project with dependencies.
    - Created `SshFtpService` to manage SFTP connections and operations.
    - Updated `README.md` with documentation for the new project.
    - Added `TTLogo.png` as a branding asset.
    - Modified solution file to include the new `CommonNetFuncs.Web.Ftp` project.

---
+++ CommonNetFuncs.Web.Ftp/README.md
@@ -0,0 +1,28 @@
+# CommonNetFuncs.Web.Ftp
+
+[![nuget](https://img.shields.io/nuget/dt/CommonNetFuncs.Web.Ftp)](https://www.nuget.org/packages/CommonNetFuncs.Web.Ftp/)
+
+This lightweight project contains helper methods for several common functions required by applications.
+
+## Contents
+
+<!-- - [Class Name](#) -->
+
+---
+
+## [Class Name Here]
+
+### Usage Examples
+
+[Description here]
+
+<details>
+<summary><h3>Usage Examples</h3></summary>
+
+#### [MethodNameHere]
+
+```cs
+//Code here
+```
+
+</details>
---
+++ CommonNetFuncs.Web.Ftp/SshFtpService.cs
@@ -0,0 +1,122 @@
+﻿
+using Renci.SshNet;
+
+namespace CommonNetFuncs.Web.Ftp;
+
+public interface ISshFtpService
+{
+    string GetHostName();
+    bool IsConnected();
+    SftpClient Connect();
+    Task<SftpClient> ConnectAsync(CancellationTokenSource? cancellationTokenSource);
+    bool DisconnectClient();
+    bool DirectoryExists(string path);
+    Task<bool> DirectoryExistsAsync(string path);
+    IEnumerable<string> GetFileList(string path, string extension = "*");
+    IAsyncEnumerable<string> GetFileListAsync(string path, string extension = "*", CancellationTokenSource? cancellationTokenSource = null);
+    Task<IEnumerable<T>> GetDataFromCsv<T>(string remoteFilePath, bool csvHasHeaderRow = true);
+    bool DeleteFile(string remoteFilePath);
+    Task<bool> DeleteFileAsync(string remoteFilePath);
+    void Dispose();
+}
+
+public sealed class SshFtpService : IDisposable, ISshFtpService
+{
+    private readonly SftpClient client;
+    private readonly FileTransferConnection connection;
+    private bool disposed;
+
+    public SshFtpService(FileTransferConnection fileTransferConnection)
+    {
+        connection = fileTransferConnection;
+        client = Connect();
+    }
+
+    public string GetHostName()
+    {
+        return connection.GetHostName();
+    }
+
+    public bool IsConnected()
+    {
+        return client.IsConnected();
+    }
+
+    public SftpClient Connect()
+    {
+        return client.Connect(connection);
+    }
+
+    public Task<SftpClient> ConnectAsync(CancellationTokenSource? cancellationTokenSource)
+    {
+        return client.ConnectAsync(connection, cancellationTokenSource);
+    }
+
+    public bool DisconnectClient()
+    {
+        return client.DisconnectClient();
+    }
+
+    public bool DirectoryExists(string path)
+    {
+        return client.DirectoryExists(path);
+    }
+
+    public Task<bool> DirectoryExistsAsync(string path)
+    {
+        return client.DirectoryExistsAsync(path);
+    }
+
+    public IEnumerable<string> GetFileList(string path, string extension = "*")
+    {
+        return client.GetFileList(path, extension);
+    }
+
+    public IAsyncEnumerable<string> GetFileListAsync(string path, string extension = "*", CancellationTokenSource? cancellationTokenSource = null)
+    {
+        return client.GetFileListAsync(path, extension, cancellationTokenSource);
+    }
+
+    public Task<IEnumerable<T>> GetDataFromCsv<T>(string remoteFilePath, bool csvHasHeaderRow = true)
+    {
+        return client.GetDataFromCsv<T>(remoteFilePath, csvHasHeaderRow);
+    }
+
+    public bool DeleteFile(string remoteFilePath)
+    {
+        return client.DeleteSftpFile(remoteFilePath);
+    }
+
+    public Task<bool> DeleteFileAsync(string remoteFilePath)
+    {
+        return client.DeleteFileAsync(remoteFilePath);
+    }
+
+    // IDisposable implementation
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                // Dispose managed resources
+                client?.Dispose();
+            }
+
+            // Dispose unmanaged resources (if any)
+
+            disposed = true;
+        }
+    }
+
+    ~SshFtpService()
+    {
+        Dispose(false);
+    }
+}

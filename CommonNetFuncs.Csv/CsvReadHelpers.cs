908243134459aa3483b5c818a8ac9f28d94ed65e
vn034157 <nick_scarpitti@na.honda.com>
2025-07-22 13:56:43 -0400

    Updated CSV reader Updated SsfFtp to use correct methods to read CSV files Updated versions

--- CommonNetFuncs.Csv/CsvReadHelpers.cs
+++ CommonNetFuncs.Csv/CsvReadHelpers.cs
@@ -8,12 +8,28 @@ namespace CommonNetFuncs.Csv;
 
 public static class CsvReadHelpers
 {
+    /// <summary>
+    /// Read from physical CSV file
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="filePath">Path to the CSV file to read from</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>List of T read from the CSV file</returns>
     public static List<T> ReadCsv<T>(string filePath, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(filePath);
         return ReadCsv<T>(reader, hasHeaders, cultureInfo);
     }
 
+    /// <summary>
+    /// Read from CSV file in a stream
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="stream">Stream of a CSV file</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>List of T read from the CSV stream</returns>
     public static List<T> ReadCsv<T>(Stream stream, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(stream);
@@ -29,6 +45,14 @@ public static class CsvReadHelpers
         return csv.GetRecords<T>().ToList();
     }
 
+    /// <summary>
+    /// Read from physical CSV file
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="filePath">Path to the CSV file to read from</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>List of T read from the CSV file</returns>
     public static async Task<List<T>> ReadCsvAsync<T>(string filePath, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(filePath);
@@ -40,6 +64,14 @@ public static class CsvReadHelpers
         return records;
     }
 
+    /// <summary>
+    /// Read from CSV file in a stream
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="stream">Stream of a CSV file</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>List of T read from the CSV stream</returns>
     public static async Task<List<T>> ReadCsvAsync<T>(Stream stream, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(stream);
@@ -51,6 +83,14 @@ public static class CsvReadHelpers
         return records;
     }
 
+    /// <summary>
+    /// Read from physical CSV file
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="filePath">Path to the CSV file to read from</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>Enumerated results of T read from the CSV file</returns>
     public static async IAsyncEnumerable<T> ReadCsvAsyncEnumerable<T>(string filePath, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(filePath);
@@ -60,6 +100,14 @@ public static class CsvReadHelpers
         }
     }
 
+    /// <summary>
+    /// Read from CSV file in a stream
+    /// </summary>
+    /// <typeparam name="T">Type to read from rows</typeparam>
+    /// <param name="stream">Stream of a CSV file</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>Enumerated results of T read from the CSV stream</returns>
     public static async IAsyncEnumerable<T> ReadCsvAsyncEnumerable<T>(Stream stream, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(stream);
@@ -82,24 +130,54 @@ public static class CsvReadHelpers
         }
     }
 
+    /// <summary>
+    /// Read from physical CSV file into a DataTable.
+    /// </summary>
+    /// <param name="filePath">Path to the CSV file to read from.</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>DataTable populated from the CSV file.</returns>
     public static DataTable ReadCsvToDataTable(string filePath, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(filePath);
         return ReadCsvToDataTable(reader, hasHeaders, null, cultureInfo);
     }
 
+    /// <summary>
+    /// Read from CSV file in a stream into a DataTable.
+    /// </summary>
+    /// <param name="stream">Stream of a CSV file.</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>DataTable populated from the CSV stream.</returns>
     public static DataTable ReadCsvToDataTable(Stream stream, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(stream);
         return ReadCsvToDataTable(reader, hasHeaders, null, cultureInfo);
     }
 
+    /// <summary>
+    /// Read from physical CSV file into a DataTable, using a specified data type for columns.
+    /// </summary>
+    /// <param name="filePath">Path to the CSV file to read from.</param>
+    /// <param name="dataType">Type to use for DataTable columns.</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>DataTable populated from the CSV file.</returns>
     public static DataTable ReadCsvToDataTable(string filePath, Type dataType, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(filePath);
         return ReadCsvToDataTable(reader, hasHeaders, dataType, cultureInfo);
     }
 
+    /// <summary>
+    /// Read from CSV file in a stream into a DataTable, using a specified data type for columns.
+    /// </summary>
+    /// <param name="stream">Stream of a CSV file.</param>
+    /// <param name="dataType">Type to use for DataTable columns.</param>
+    /// <param name="hasHeaders">Optional: Indicates file has headers. Default is true.</param>
+    /// <param name="cultureInfo">Optional: Culture to read file with. Default is invariant culture.</param>
+    /// <returns>DataTable populated from the CSV stream.</returns>
     public static DataTable ReadCsvToDataTable(Stream stream, Type dataType, bool hasHeaders = true, CultureInfo? cultureInfo = null)
     {
         using StreamReader reader = new(stream);

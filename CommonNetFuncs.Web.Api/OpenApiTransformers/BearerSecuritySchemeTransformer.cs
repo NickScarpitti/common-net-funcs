9021350727cc797a85aa556e63629ac1a0560c7c
vn034157 <nick_scarpitti@na.honda.com>
2025-08-25 10:27:58 -0400

    Added open API transformers

---
+++ CommonNetFuncs.Web.Api/OpenApiTransformers/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace CommonNetFuncs.Web.Api.OpenApiTransformers;
+
+public sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider, string? authenticationSchemeName = "Bearer") : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        IEnumerable<AuthenticationScheme> authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync().ConfigureAwait(false);
+
+        if (authenticationSchemeName == null || authenticationSchemes.Any(authScheme => authScheme.Name == authenticationSchemeName))
+        {
+            document.Components ??= new OpenApiComponents();
+
+            const string securitySchemeId = "Bearer";
+
+            document.Components.SecuritySchemes.Add(securitySchemeId, new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                In = ParameterLocation.Header,
+                BearerFormat = "Json Web Token"
+            });
+
+            // Add "Bearer" scheme as a requirement for the API as a whole
+            document.SecurityRequirements.Add(new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = securitySchemeId, Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
+            });
+        }
+    }
+}
---
+++ CommonNetFuncs.Web.Api/OpenApiTransformers/HeaderTransformer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace CommonNetFuncs.Web.Api.OpenApiTransformers;
+
+internal sealed class HeaderTransformer() : IOpenApiOperationTransformer
+{
+    public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+    {
+        operation.Parameters ??= new List<OpenApiParameter>();
+        operation.Parameters.Add(new OpenApiParameter
+        {
+            Name = "Accept",
+            In = ParameterLocation.Header,
+            Required = true,
+            Schema = new OpenApiSchema
+            {
+                Type = "string",
+                Default = new OpenApiString("application/json")
+            }
+        });
+        return Task.CompletedTask;
+    }
+}

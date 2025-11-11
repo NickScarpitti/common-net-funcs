2406487bdd60907cf82b12c990f98e7b3d6e8116
vn034157 <nick_scarpitti@na.honda.com>
2025-11-11 09:01:29 -0500

    Updated Packages

--- CommonNetFuncs.Web.Api/OpenApiTransformers/HeaderTransformer.cs
+++ CommonNetFuncs.Web.Api/OpenApiTransformers/HeaderTransformer.cs
@@ -5,20 +5,20 @@ namespace CommonNetFuncs.Web.Api.OpenApiTransformers;
 
 public sealed class HeaderTransformer : IOpenApiOperationTransformer
 {
-  public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
-  {
-    operation.Parameters ??= new List<OpenApiParameter>();
-    operation.Parameters.Add(new OpenApiParameter
-        {
-            Name = "Accept",
-            In = ParameterLocation.Header,
-            Required = true,
-            Schema = new OpenApiSchema
-            {
-                Type = "string",
-                Default = new Microsoft.OpenApi.Any.OpenApiString("application/json")
-            }
-        });
-    return Task.CompletedTask;
-  }
+	public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+	{
+		operation.Parameters ??= new List<OpenApiParameter>();
+		operation.Parameters.Add(new OpenApiParameter
+		{
+			Name = "Accept",
+			In = ParameterLocation.Header,
+			Required = true,
+			Schema = new OpenApiSchema
+			{
+				Type = "string",
+				Default = new Microsoft.OpenApi.Any.OpenApiString("application/json")
+			}
+		});
+		return Task.CompletedTask;
+	}
 }
--- Web.Api.Tests/OpenApiTransformers/OpenApiTransformerTests.cs
+++ Web.Api.Tests/OpenApiTransformers/OpenApiTransformerTests.cs
@@ -10,135 +10,135 @@ namespace Web.Api.Tests.OpenApiTransformers;
 #pragma warning disable CRR0029 // ConfigureAwait(true) is called implicitly
 public sealed class OpenApiTransformerTests
 {
-    [Theory]
-    [InlineData(null)]
-    [InlineData(0)]
-    [InlineData(1)]
-    public async Task HeaderTransformer_AddsAcceptHeaderParameter(object? initialParamCount)
-    {
-        // Arrange
-        HeaderTransformer transformer = new();
-        OpenApiOperation operation = new()
-        {
-            Parameters = initialParamCount switch
-            {
-                null => null,
-                0 => new List<OpenApiParameter>(),
-                1 => new List<OpenApiParameter> { new() { Name = "X-Test", In = ParameterLocation.Header } },
-                _ => null
-            }
-        };
-        CancellationToken cancellationToken = CancellationToken.None;
+	[Theory]
+	[InlineData(null)]
+	[InlineData(0)]
+	[InlineData(1)]
+	public async Task HeaderTransformer_AddsAcceptHeaderParameter(object? initialParamCount)
+	{
+		// Arrange
+		HeaderTransformer transformer = new();
+		OpenApiOperation operation = new()
+		{
+			Parameters = initialParamCount switch
+			{
+				null => null,
+				0 => new List<OpenApiParameter>(),
+				1 => new List<OpenApiParameter> { new() { Name = "X-Test", In = ParameterLocation.Header } },
+				_ => null
+			}
+		};
+		CancellationToken cancellationToken = CancellationToken.None;
 
-        // Act
-        await transformer.TransformAsync(operation, null!, cancellationToken);
+		// Act
+		await transformer.TransformAsync(operation, null!, cancellationToken);
 
-        // Assert
-        operation.Parameters.ShouldNotBeNull();
-        operation.Parameters.ShouldContain(p => p.Name == "Accept" && p.In == ParameterLocation.Header);
-        OpenApiParameter? acceptParam = operation.Parameters.FirstOrDefault(p => p.Name == "Accept");
-        acceptParam.ShouldNotBeNull();
-        acceptParam!.Required.ShouldBeTrue();
-        acceptParam.Schema.ShouldNotBeNull();
-        acceptParam.Schema.Type.ShouldBe("string");
-        acceptParam.Schema.Default.ShouldBeOfType<OpenApiString>();
-        ((OpenApiString)acceptParam.Schema.Default).Value.ShouldBe("application/json");
-    }
+		// Assert
+		operation.Parameters.ShouldNotBeNull();
+		operation.Parameters.ShouldContain(p => p.Name == "Accept" && p.In == ParameterLocation.Header);
+		OpenApiParameter? acceptParam = operation.Parameters.FirstOrDefault(p => p.Name == "Accept");
+		acceptParam.ShouldNotBeNull();
+		acceptParam!.Required.ShouldBeTrue();
+		acceptParam.Schema.ShouldNotBeNull();
+		acceptParam.Schema.Type.ShouldBe("string");
+		acceptParam.Schema.Default.ShouldBeOfType<OpenApiString>();
+		((OpenApiString)acceptParam.Schema.Default).Value.ShouldBe("application/json");
+	}
 
-    [Theory]
-    [InlineData(null)]
-    [InlineData("Bearer")]
-    [InlineData("Other")]
-    public async Task BearerSecuritySchemeTransformer_TransformAsync_AddsSecurityScheme_WhenAppropriate(string? schemeName)
-    {
-        // Arrange
-        IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
-        List<AuthenticationScheme> schemes = new()
-        {
-            new AuthenticationScheme("Bearer", "Bearer", typeof(IAuthenticationHandler)),
-            new AuthenticationScheme("Other", "Other", typeof(IAuthenticationHandler))
-        };
-        A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(schemes));
-        IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = schemeName ?? "Bearer" });
-        BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
-        OpenApiDocument document = new();
-        OpenApiDocumentTransformerContext context = new()
-        {
-            DocumentName = "TestDoc",
-            DescriptionGroups = [],
-            ApplicationServices = null!
-        };
-        CancellationToken cancellationToken = CancellationToken.None;
+	[Theory]
+	[InlineData(null)]
+	[InlineData("Bearer")]
+	[InlineData("Other")]
+	public async Task BearerSecuritySchemeTransformer_TransformAsync_AddsSecurityScheme_WhenAppropriate(string? schemeName)
+	{
+		// Arrange
+		IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
+		List<AuthenticationScheme> schemes = new()
+				{
+						new AuthenticationScheme("Bearer", "Bearer", typeof(IAuthenticationHandler)),
+						new AuthenticationScheme("Other", "Other", typeof(IAuthenticationHandler))
+				};
+		A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(schemes));
+		IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = schemeName ?? "Bearer" });
+		BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
+		OpenApiDocument document = new();
+		OpenApiDocumentTransformerContext context = new()
+		{
+			DocumentName = "TestDoc",
+			DescriptionGroups = [],
+			ApplicationServices = null!
+		};
+		CancellationToken cancellationToken = CancellationToken.None;
 
-        // Act
-        await transformer.TransformAsync(document, context, cancellationToken);
+		// Act
+		await transformer.TransformAsync(document, context, cancellationToken);
 
-        // Assert
-        document.Components.ShouldNotBeNull();
-        document.Components.SecuritySchemes.ShouldContainKey("Bearer");
-        OpenApiSecurityScheme scheme = document.Components.SecuritySchemes["Bearer"];
-        scheme.Type.ShouldBe(SecuritySchemeType.Http);
-        scheme.Scheme.ShouldBe("bearer");
-        scheme.In.ShouldBe(ParameterLocation.Header);
-        scheme.BearerFormat.ShouldBe("Json Web Token");
-        document.SecurityRequirements.ShouldNotBeEmpty();
-        document.SecurityRequirements.Any(req => req.Keys.Any(k => k.Reference?.Id == "Bearer" && k.Reference.Type == ReferenceType.SecurityScheme)).ShouldBeTrue();
-    }
+		// Assert
+		document.Components.ShouldNotBeNull();
+		document.Components.SecuritySchemes.ShouldContainKey("Bearer");
+		OpenApiSecurityScheme scheme = document.Components.SecuritySchemes["Bearer"];
+		scheme.Type.ShouldBe(SecuritySchemeType.Http);
+		scheme.Scheme.ShouldBe("bearer");
+		scheme.In.ShouldBe(ParameterLocation.Header);
+		scheme.BearerFormat.ShouldBe("Json Web Token");
+		document.SecurityRequirements.ShouldNotBeEmpty();
+		document.SecurityRequirements.Any(req => req.Keys.Any(k => k.Reference?.Id == "Bearer" && k.Reference.Type == ReferenceType.SecurityScheme)).ShouldBeTrue();
+	}
 
-    [Theory]
-    [InlineData("NonExistentScheme")]
-    public async Task BearerSecuritySchemeTransformer_TransformAsync_DoesNotAdd_WhenNoMatchingScheme(string schemeName)
-    {
-        // Arrange
-        IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
-        List<AuthenticationScheme> schemes = new()
-        {
-            new AuthenticationScheme("Bearer", "Bearer", typeof(IAuthenticationHandler)),
-            new AuthenticationScheme("Other", "Other", typeof(IAuthenticationHandler))
-        };
-        A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(schemes));
-        IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = schemeName });
-        BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
-        OpenApiDocument document = new();
-        OpenApiDocumentTransformerContext context = new()
-        {
-            DocumentName = "TestDoc",
-            DescriptionGroups = [],
-            ApplicationServices = null!
-        };
-        CancellationToken cancellationToken = CancellationToken.None;
+	[Theory]
+	[InlineData("NonExistentScheme")]
+	public async Task BearerSecuritySchemeTransformer_TransformAsync_DoesNotAdd_WhenNoMatchingScheme(string schemeName)
+	{
+		// Arrange
+		IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
+		List<AuthenticationScheme> schemes = new()
+				{
+						new AuthenticationScheme("Bearer", "Bearer", typeof(IAuthenticationHandler)),
+						new AuthenticationScheme("Other", "Other", typeof(IAuthenticationHandler))
+				};
+		A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(schemes));
+		IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = schemeName });
+		BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
+		OpenApiDocument document = new();
+		OpenApiDocumentTransformerContext context = new()
+		{
+			DocumentName = "TestDoc",
+			DescriptionGroups = [],
+			ApplicationServices = null!
+		};
+		CancellationToken cancellationToken = CancellationToken.None;
 
-        // Act
-        await transformer.TransformAsync(document, context, cancellationToken);
+		// Act
+		await transformer.TransformAsync(document, context, cancellationToken);
 
-        // Assert
-        document.Components.ShouldBeNull();
-        document.SecurityRequirements.ShouldBeEmpty();
-    }
+		// Assert
+		document.Components.ShouldBeNull();
+		document.SecurityRequirements.ShouldBeEmpty();
+	}
 
-    [Fact]
-    public async Task BearerSecuritySchemeTransformer_TransformAsync_DoesNotAdd_WhenNoSchemes()
-    {
-        // Arrange
-        IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
-        A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(new List<AuthenticationScheme>()));
-        IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = "Bearer" });
-        BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
-        OpenApiDocument document = new();
-        OpenApiDocumentTransformerContext context = new()
-        {
-            DocumentName = "TestDoc",
-            DescriptionGroups = [],
-            ApplicationServices = null!
-        };
-        CancellationToken cancellationToken = CancellationToken.None;
+	[Fact]
+	public async Task BearerSecuritySchemeTransformer_TransformAsync_DoesNotAdd_WhenNoSchemes()
+	{
+		// Arrange
+		IAuthenticationSchemeProvider fakeProvider = A.Fake<IAuthenticationSchemeProvider>();
+		A.CallTo(() => fakeProvider.GetAllSchemesAsync()).Returns(Task.FromResult<IEnumerable<AuthenticationScheme>>(new List<AuthenticationScheme>()));
+		IOptions<BearerSecuritySchemeOptions> options = Options.Create(new BearerSecuritySchemeOptions { AuthenticationSchemeName = "Bearer" });
+		BearerSecuritySchemeTransformer transformer = new(fakeProvider, options);
+		OpenApiDocument document = new();
+		OpenApiDocumentTransformerContext context = new()
+		{
+			DocumentName = "TestDoc",
+			DescriptionGroups = [],
+			ApplicationServices = null!
+		};
+		CancellationToken cancellationToken = CancellationToken.None;
 
-        // Act
-        await transformer.TransformAsync(document, context, cancellationToken);
+		// Act
+		await transformer.TransformAsync(document, context, cancellationToken);
 
-        // Assert
-        document.Components.ShouldBeNull();
-        document.SecurityRequirements.ShouldBeEmpty();
-    }
+		// Assert
+		document.Components.ShouldBeNull();
+		document.SecurityRequirements.ShouldBeEmpty();
+	}
 }
 #pragma warning restore CRR0029 // ConfigureAwait(true) is called implicitly

53e3b021da9bc90a8fc7031cc852d3be203b007b
vn034157 <nick_scarpitti@na.honda.com>
2026-03-02 08:39:46 -0500

    Updated packages Removed unused test code Added code coverage

--- EFCore.Tests/TestContext.cs
+++ EFCore.Tests/TestContext.cs
@@ -5,13 +5,12 @@ namespace EFCore.Tests;
 
 
 // Test types
+
 public class TestDbContext(DbContextOptions<TestDbContext> options) : DbContext(options)
 {
 	public DbSet<TestEntity> TestEntities => Set<TestEntity>();
 
 	public DbSet<TestEntityWithCompoundKey> TestEntitiesWithCompoundKey => Set<TestEntityWithCompoundKey>();
-
-	public DbSet<TestEntityWithDateTimeOffset> TestEntitiesWithDateTimeOffset => Set<TestEntityWithDateTimeOffset>();
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
@@ -28,9 +27,6 @@ public class TestDbContext(DbContextOptions<TestDbContext> options) : DbContext(
 
 		modelBuilder.Entity<TestEntityWithCompoundKey>()
 			.HasKey(e => new { e.Key1, e.Key2 });
-
-		modelBuilder.Entity<TestEntityWithDateTimeOffset>()
-			.HasKey(e => e.Id);
 	}
 }
 
@@ -70,59 +66,8 @@ public class TestEntityWithCompoundKey
 	public required string Name { get; set; }
 }
 
-public class TestEntityWithDateTimeOffset
-{
-	public int Id { get; set; }
-
-	public required string Name { get; set; }
-
-	public DateTimeOffset CreatedDate { get; set; }
-}
-
-// Circular reference test types
-public class CircularRefDbContext(DbContextOptions<CircularRefDbContext> options) : DbContext(options)
-{
-	public DbSet<ParentEntity> Parents => Set<ParentEntity>();
-
-	public DbSet<ChildEntity> Children => Set<ChildEntity>();
-
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.Entity<ParentEntity>()
-			.HasKey(e => e.Id);
-
-		modelBuilder.Entity<ChildEntity>()
-			.HasKey(e => e.Id);
-
-		modelBuilder.Entity<ChildEntity>()
-			.HasOne(c => c.Parent)
-			.WithMany(p => p.Children)
-			.HasForeignKey(c => c.ParentId)
-			.OnDelete(DeleteBehavior.Cascade);
-	}
-}
-
-public class ParentEntity
-{
-	public int Id { get; set; }
-
-	public required string Name { get; set; }
-
-	public ICollection<ChildEntity> Children { get; set; } = new List<ChildEntity>();
-}
-
-public class ChildEntity
-{
-	public int Id { get; set; }
-
-	public required string Name { get; set; }
-
-	public int ParentId { get; set; }
-
-	public ParentEntity? Parent { get; set; }
-}
-
 // Context and entity for GlobalFilterOptions tests to avoid cache interference
+
 public class TestDbContextForFilters(DbContextOptions<TestDbContextForFilters> options) : DbContext(options)
 {
 	public DbSet<TestEntityForFilters> TestEntities => Set<TestEntityForFilters>();
@@ -144,51 +89,4 @@ public class TestEntityForFilters
 	public string? Description { get; set; }
 
 	public DateTime CreatedDate { get; set; }
-}
-
-// Context and entities for navigation property tests to avoid cache interference
-public class TestDbContextForNavigation(DbContextOptions<TestDbContextForNavigation> options) : DbContext(options)
-{
-	public DbSet<TestEntityForNavigation> TestEntities => Set<TestEntityForNavigation>();
-
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.Entity<TestEntityForNavigation>()
-			.HasKey(e => e.Id);
-
-		modelBuilder.Entity<TestEntityDetailForNavigation>()
-			.HasKey(e => e.Id);
-
-		modelBuilder.Entity<TestEntityDetailForNavigation>()
-			.HasOne(e => e.TestEntity)
-			.WithMany(e => e.Details)
-			.HasForeignKey(e => e.TestEntityId);
-	}
-}
-
-public class TestEntityForNavigation
-{
-	public int Id { get; set; }
-
-	public required string Name { get; set; }
-
-	public int Value { get; set; }
-
-	public string? Description { get; set; }
-
-	public DateTime CreatedDate { get; set; }
-
-	public ICollection<TestEntityDetailForNavigation>? Details { get; set; }
-}
-
-public class TestEntityDetailForNavigation
-{
-	public int Id { get; set; }
-
-	public required string Description { get; set; }
-
-	public int TestEntityId { get; set; }
-
-	[JsonIgnore]
-	public TestEntityForNavigation? TestEntity { get; set; }
 }
